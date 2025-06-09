8539f2bc5adba3609e0c1a92983591a4247a2fa9
Michael CARBENAY <michael@altazion.com>
2025-06-09 19:51:46 +0200

    Création du projet frontend

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+/* Ajoutez ici vos styles globaux ou spécifiques à l'app */
+</style>
